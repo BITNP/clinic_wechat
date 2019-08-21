cb1af389391dd4444cb84ebf505cf6833e7530ac
FKYnJYQ <loveress01@gmail.com>
2019-08-21 21:12:05 +0800

    feat: 增加TOC，公告栏

--- records/src/views/MainList.vue
+++ records/src/views/MainList.vue
@@ -3,41 +3,75 @@
     <tab>
       <tab-item @on-item-click="tabNum = 0" selected>正在处理</tab-item>
       <tab-item @on-item-click="tabNum = 1">已处理</tab-item>
+      <tab-item @on-item-click="tabNum = 2">公告</tab-item>
     </tab>
-    <template v-for="(d,index) in filtered_records">
-      <!-- {{d}} -->
-      <FormPreview
-        header-label="工单状态"
-        :key="index"
-        :header-value="STATUS_MAP[d.status]"
-        :body-items="dict2list(d)"
-        :footer-buttons="d.status<=5?dict2button(d):[]"
-        :name="[d,index]"
-      />
-      <br :key="index + 'd'" />
+    <template v-if="tabNum < 2">
+      <template v-for="(d,index) in filtered_records">
+        <FormPreview
+          header-label="工单状态"
+          :key="index"
+          :header-value="STATUS_MAP[d.status]"
+          :body-items="dict2list(d)"
+          :footer-buttons="WORKING_STATUS.includes(d.status)?dict2button(d):[]"
+          :name="[d,index]"
+        />
+        <br :key="index + 'd'" />
+      </template>
+
+      <infinite-loading @infinite="loadMore" spinner="circles">
+        <template slot="no-results">
+          <divider>没有更多数据咧</divider>
+        </template>
+        <template slot="no-more">
+          <divider>已经到底啦 :)</divider>
+        </template>
+      </infinite-loading>
+
+      <br />
+      <Box gap="10px 10px" v-if="tabNum == 0">
+        <x-button type="primary" action-type="button" link="/new">新建工单</x-button>
+      </Box>
+      <br />
+
+      <br />
+      <div v-transfer-dom>
+        <popup v-model="display">
+          <Popupcontent v-model="record" :create="false" />
+        </popup>
+      </div>
     </template>
+    <template v-else>
+      <template v-for="(ann,idx) in announcements">
+        <Card :key="idx">
+          <div slot="header">
+            <group>
+              <Cell title="标题" :value="ann.title"></Cell>
+              <Cell title="公告类别" :value="ann.title"></Cell>
+            </group>
+          </div>
+          <template slot="content">
+            <group>
+              <Cell title="内容" @click.native="ann.show = !ann.show">
+                <x-icon type="ios-arrow-down" size="30" v-if="ann.show"></x-icon>
+                <x-icon type="ios-arrow-up" size="30" v-if="!ann.show"></x-icon>
+              </Cell>
+            </group>
+            <p slot="content" class="card-padding" v-if="ann.show">
+              <vue-markdown :show="ann.content.length>0" :source="ann.content"></vue-markdown>
+              {{ann.content}}
+            </p>
+          </template>
 
-    <infinite-loading @infinite="loadMore" spinner="circles">
-      <template slot="no-results">
-        <divider>没有更多数据咧</divider>
+          <div slot="footer">
+            <group>
+              <Cell title="最后更新时间" :value="new Date(ann.lastEditedTime).toLocaleDateString()"></Cell>
+              <Cell title="过期时间" :value="new Date(ann.expireDate).toLocaleDateString()"></Cell>
+            </group>
+          </div>
+        </Card>
+        <br :key="idx" />
       </template>
-      <template slot="no-more">
-        <divider>已经到底啦 :)</divider>
-      </template>
-    </infinite-loading>
-
-    <br />
-    <Box gap="10px 10px" v-if="tabNum == 0">
-      <x-button type="primary" action-type="button" link="/new">新建工单</x-button>
-    </Box>
-    <br />
-
-    <br />
-    <div v-transfer-dom>
-      <popup v-model="display">
-        <Popupcontent v-model="record" :create="false" />
-      </popup>
-    </div>
+    </template>
   </div>
 </template>
 
@@ -51,10 +85,12 @@ import {
   Card,
   XButton,
   Divider,
-  Box
+  Box,
+  Cell
 } from 'vux'
 import InfiniteLoading from 'vue-infinite-loading'
 import Popupcontent from '@/components/PopupContent'
+import VueMarkdown from 'vue-markdown'
 
 export default {
   components: {
@@ -68,7 +104,9 @@ export default {
     XButton,
     Divider,
     InfiniteLoading,
-    Box
+    Box,
+    VueMarkdown,
+    Cell
   },
   data: () => ({
     tabNum: 0,
@@ -82,6 +120,9 @@ export default {
     },
     record_index: -1,
     all_records: [],
+    announcements: [],
+    WORKING_STATUS: [0, 1, 2, 4, 5],
+    FINISHED_STATUS: [3, 6, 7, 8],
     STATUS_MAP: {
       0: '上单问题未解决',
       1: '预约待确认',
@@ -109,14 +150,22 @@ export default {
     CAMPUS_MAP: {
       LX: '良乡',
       ZGC: '中关村'
+    },
+    ANNOUNCEMENTS_MAP: {
+      TOC: '免责声明',
+      CM: '普通公告'
     }
   }),
   computed: {
     filtered_records () {
       if (this.tabNum === 0) {
-        return this.all_records.filter(r => r.status <= 5)
+        return this.all_records.filter(r =>
+          this.WORKING_STATUS.includes(r.status)
+        )
       } else {
-        return this.all_records.filter(r => r.status > 5)
+        return this.all_records.filter(
+          r => !this.WORKING_STATUS.includes(r.status)
+        )
       }
     }
   },
@@ -174,10 +223,21 @@ export default {
     },
     initRecords () {
       this.next = this.Const + 'wechat/'
+    },
+    initAnnouncements () {
+      console.log('init announce')
+      this.$http.get(`${this.Const}announcement/`).then(({ data }) => {
+        data.map(v => {
+          v.show = false
+        })
+        this.announcements = data
+        console.log(this.announcements)
+      })
     }
   },
   mounted () {
     this.initRecords()
+    this.initAnnouncements()
   }
 }
 </script>
@@ -189,5 +249,9 @@ export default {
 .logo {
   width: 100px;
   height: 100px;
+}
+.card-padding {
+  padding: 15px;
+  margin: 40px;
 }
 </style>
