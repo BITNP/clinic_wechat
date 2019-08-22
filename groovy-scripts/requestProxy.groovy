4f71082e0f81570be88e35578e76b4bd70ed4bb6
FKYnJYQ <loveress01@gmail.com>
2019-08-22 13:59:52 +0800

    update groovy-script

--- groovy-scripts/requestProxy.groovy
+++ groovy-scripts/requestProxy.groovy
@@ -1,28 +1,64 @@
 import wslite.rest.*
-
+// 作为代理
 // 根据所选内容变化
-String API_HOST = "http://clinic.bitnp.net/api/wechat/2/"
+String API_HOST = "http://clinic.bitnp.net/"
+API_HOST = API_HOST + request.getRequestURI()
+// e.g. http://clinic.bitnp.net/api/wechat/
 
+// set apikey
+def HEADERS = [apikey: "oh-my-tlb"]
+
+// get badgeNumber
 String badgeNumber = follower?.badgeNumber
-// badgeNumber = '1120143205'
-String appointmentTime = params?.appointment_time
-
 if (!badgeNumber) {
     return ['errcode': 1]
 }
 
+// set query for backend authentication
+def QUERY = [username: badgeNumber]
+
+// badgeNumber = '1120143205'
+
+
 def client = new RESTClient(API_HOST)
-client.httpClient.sslTrustAllCerts = true
+// client.httpClient.sslTrustAllCerts = true
 
 try {
-    def data = client.post(
-            headers: ['apikey': 'oh-my-tlb'],
-            query: [username: badgeNumber],
+
+    def method = request.getMethod()
+
+    if (request.method == "post"){
+        def data = client.post(
+            headers: HEADERS,
+            query: QUERY,
             requestContentType: 'application/json',
-            body: [appointment_time:appointmentTime,
-            campus:campus,description:description,phone_num:phone_num,
-            realname:realname]
-    ).json
+            body: request.JSON
+        ).json
+    } else if (request.method == "get"){
+        def data = client.get(
+            headers: HEADERS,
+            query: QUERY,
+            requestContentType: 'application/json',
+            body: request.JSON
+    } else if (request.method == "delete"){
+        def data = client.delete(
+            headers: HEADERS,
+            query: QUERY,
+            requestContentType: 'application/json',
+            body: request.JSON
+    } else if (request.method == "put"){
+        def data = client.put(
+            headers: HEADERS,
+            query: QUERY,
+            requestContentType: 'application/json',
+            body: request.JSON
+    } else if (request.method == "option"){
+        def data = client.option(
+            headers: HEADERS,
+            query: QUERY,
+            requestContentType: 'application/json',
+            body: request.JSON
+    }
 
     return data
 } catch (e) {
