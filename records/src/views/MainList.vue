43b4c52cd879e5f3df18105f62daf1ff11641bfb
FKYnJYQ <loveress01@gmail.com>
2019-10-12 12:46:19 +0800

    feat(announcements): 增加二级目录
    fix #34.

--- records/src/views/MainList.vue
+++ records/src/views/MainList.vue
@@ -51,35 +51,7 @@
           v-model="announcements_order"
         ></popup-radio>
       </group> -->
-      <template v-for="(ann,idx) in ordered_announcements">
-        <Card :key="idx+'c'">
-          <div slot="header">
-            <group>
-              <Cell title="标题" :value="ann.title"></Cell>
-              <Cell title="公告类别" :value="ann.title"></Cell>
-            </group>
-          </div>
-          <template slot="content">
-            <group>
-              <!-- 可以修改为可折叠的？ -->
-              <Cell title="内容">
-              </Cell>
-            </group>
-            <p slot="content" class="card-padding">
-              <vue-markdown :show="ann.content.length>0" :source="ann.content"></vue-markdown>
-            </p>
-          </template>
-
-          <div slot="footer">
-            <group>
-              <Cell title="最后更新时间" :value="new Date(ann.lastEditedTime).toLocaleDateString()"></Cell>
-              <Cell title="过期时间" :value="new Date(ann.expireDate).toLocaleDateString()"></Cell>
-            </group>
-          </div>
-        </Card>
-        <br :key="idx+'b'" />
-      </template>
-      <divider>网络开拓者协会 - 电脑诊所</divider>
+     <Announcements></Announcements>
     </template>
     <template v-else>
       <Calendar :dates="dates" />
@@ -107,29 +79,9 @@ import {
 } from 'vux'
 import InfiniteLoading from 'vue-infinite-loading'
 import Popupcontent from '@/components/PopupContent'
-import VueMarkdown from 'vue-markdown'
 import Calendar from '@/components/Calendar'
 import RecordPreview from '@/components/RecordPreview'
-
-// let cmpHelper = function (attrName, less = false) {
-//   if (less) {
-//     return (a, b) => {
-//       if (a[attrName] > b[attrName]) {
-//         return 1
-//       } else if (a[attrName] < b[attrName]) {
-//         return -1
-//       } else return 0
-//     }
-//   } else {
-//     return (a, b) => {
-//       if (a[attrName] > b[attrName]) {
-//         return -1
-//       } else if (a[attrName] < b[attrName]) {
-//         return 1
-//       } else return 0
-//     }
-//   }
-// }
+import Announcements from '@/components/Announcements'
 
 export default {
   components: {
@@ -144,7 +96,6 @@ export default {
     Divider,
     InfiniteLoading,
     Box,
-    VueMarkdown,
     Cell,
     Group,
     Calendar,
@@ -152,17 +103,13 @@ export default {
     Flow,
     FlowState,
     FlowLine,
-    RecordPreview
+    RecordPreview,
+    Announcements
   },
   data: () => ({
     tabTitles: ['诊所服务日历', '正在处理', '已处理', '公告'],
-    debug_more: '',
-    debug_announcements: '',
-    debug_request: '',
-    debug_request_data: '',
     tabNum: 0,
     display_new_form: false,
-    announcements_order: '优先级',
     scroll: false,
     display: false,
     dates: [],
@@ -176,26 +123,13 @@ export default {
     },
     record_index: -1,
     all_records: [],
-    announcements: [],
     WORKING_STATUS: [0, 1, 2, 4, 5],
     FINISHED_STATUS: [3, 6, 7, 8],
     CAMPUS_MAP: {
       LX: '良乡',
       ZGC: '中关村'
-    },
-    ANNOUNCEMENTS_MAP: {
-      TOC: '免责声明',
-      CM: '普通公告'
-    },
-    ANNOUNCEMENTS_ORDER_OPTIONS: [
-      '优先级',
-      '最新创建',
-      '最早创建',
-      '最新修改',
-      '最早修改',
-      '最晚过期',
-      '最早过期'
-    ]
+    }
+
   }),
   computed: {
     filtered_records () {
@@ -209,26 +143,7 @@ export default {
         )
       }
     }
-    // ordered_announcements () {
-    //   let sortby
-    //   if (this.announcements_order === '优先级') {
-    //     sortby = cmpHelper('priority', true)
-    //   } else if (this.announcements_order === '最新创建') {
-    //     sortby = cmpHelper('createdTime')
-    //   } else if (this.announcements_order === '最早创建') {
-    //     sortby = cmpHelper('createdTime', true)
-    //   } else if (this.announcements_order === '最新修改') {
-    //     sortby = cmpHelper('lastEditedTime')
-    //   } else if (this.announcements_order === '最早修改') {
-    //     sortby = cmpHelper('lastEditedTime', true)
-    //   } else if (this.announcements_order === '最晚过期') {
-    //     sortby = cmpHelper('expireTime')
-    //   } else {
-    //     // 最早过期
-    //     sortby = cmpHelper('expireTime', true)
-    //   }
-    //   return this.announcements.sort(sortby)
-    // }
+
   },
   methods: {
     // touchStart (e) {
@@ -278,36 +193,13 @@ export default {
     },
     initRecords () {
       this.next = this.Const + 'wechat/'
-    },
-    initAnnouncements () {
-      console.log('init announce')
-      this.$http
-        .get(`${this.Const}announcement/`)
-        .then(({ data }) => {
-          this.debug_announcements = data
-          data.map(v => {
-            v.show = false
-          })
-          this.announcements = data
-          console.log(this.announcements)
-        })
-        .catch(e => {
-          if (e.request && e.request.data) {
-            this.debug_request = e.request
-            this.debug_request_data = e.request.data
-          } else if (e && e.request) {
-            this.debug_request = e
-            this.debug_request_data = e.request
-          } else {
-            this.debug_request = e
-          }
-        })
     }
+
   },
   created () {
     // 放到Store里，还可以进行性能优化
     this.initRecords()
-    this.initAnnouncements()
+    this.$store.commit('initAnnouncements')
   },
   mounted () {
     this.record.campus = '良乡校区'
