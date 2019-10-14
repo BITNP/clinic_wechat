80af8b81b99fed4011496be91cef1562d55d2ec7
FKYnJYQ <loveress01@gmail.com>
2019-10-14 14:09:49 +0800

    feat(packaging): 提供打包时修改apikey的方法

--- packing.sh
+++ packing.sh
@@ -6,6 +6,7 @@ mkdir -p package/data-api
 mkdir -p package/page
 cp -r records/dist/* package/page
 cp groovy-scripts/* package/data-api/
+bash rename-api-key.sh # you should defile the file by yourself to change apikey in groovy script
 cp misc/lightapp.json package/lightapp.json
 
 mv package/page/index.html package/page/pcClinic.html
