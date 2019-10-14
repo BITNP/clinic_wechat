45721729aa45266a61aeba601c47c1525752e371
FKYnJYQ <loveress01@gmail.com>
2019-10-14 13:59:47 +0800

    fix(recordform): 更准确的字数限制提示
    更准确的字数限制提示
    
    fix #39.

--- records/src/components/RecordForm.vue
+++ records/src/components/RecordForm.vue
@@ -22,18 +22,20 @@
         title="问题描述"
         placeholder="请尽可能详细地描述您所遇到的问题"
         required
-        :max="300"
+        :max="600"
         v-model="record.description"
       ></x-textarea>
     </group>
     <group title="选填项，这将有助于我们提供更好的服务">
-      <x-input title="电脑型号" text-align="right" v-model="record.model"></x-input>
+      <x-input title="电脑型号" text-align="right" v-model="record.model"
+      max="200"></x-input>
       <x-input title="手机号" is-type="china-mobile" text-align="right" v-model="record.phone_num"></x-input>
-      <x-input title="真实姓名" text-align="right" v-model="record.realname"></x-input>
+      <x-input title="真实姓名" text-align="right" v-model="record.realname" max="50"></x-input>
       <x-input
         title="电脑开机密码"
         placeholder="帮助我们唤醒休眠的电脑"
         text-align="right"
+        max="256"
         v-model="record.password"
       ></x-input>
     </group>
