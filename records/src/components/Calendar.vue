75f3d5f45f6b35876b10b92486669f4e8f7fac67
FKYnJYQ <loveress01@gmail.com>
2019-10-15 09:11:57 +0800

    fix(recordform): 修复max变量类型 string -> int

--- records/src/components/Calendar.vue
+++ records/src/components/Calendar.vue
@@ -49,7 +49,7 @@
 </template>
 
 <script>
-import { InlineCalendar, XTable, Divider, Flexbox, FlexboxItem } from 'vux';
+import { InlineCalendar, XTable, Divider, Flexbox, FlexboxItem } from 'vux'
 export default {
   components: { InlineCalendar, XTable, Divider, Flexbox, FlexboxItem },
   data: () => ({ value: null }),
